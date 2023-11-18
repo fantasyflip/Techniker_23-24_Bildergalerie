47d4593fce94c13e4b43e6d498abbe29776f5f2a
FantasyFlip <>
2023-11-18 11:23:38 +0100

    feat: random unsplash images gallery

---
+++ script.js
@@ -0,0 +1,21 @@
+function generateImageElements(amount) {
+  let document = window.document;
+  let galleryParent = document.getElementById("gallery-wrapper");
+  for (let i = 0; i < amount; i++) {
+    let image = document.createElement("img");
+
+    fetch(`https://source.unsplash.com/user/fantasyflip`).then((response) => {
+      image.src = response.url;
+      image.alt =
+        "Random image from unsplash by user Philipp Katzenberger - " + i;
+      image.classList.add("gallery-item");
+
+      galleryParent.appendChild(image);
+    });
+  }
+}
+
+//Initial Theme Setup
+window.addEventListener("load", function () {
+  generateImageElements(20);
+});
